37e1e7abf8e59d71eef53bd35065226cc38dabfc
FunmiAkitoye <tofakitoye@gmail.com>
2026-05-15 12:38:39 +0100

    Created the relevant pages

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+import { registerUser } from "../services/authService";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await registerUser(formData);
+
+      console.log(data);
+
+      alert("Registration Successful");
+    } catch (error) {
+      console.log(error);
+
+      alert("Registration Failed");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          placeholder="Full Name"
+          onChange={handleChange}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]); // Forces Google DNS to resolve MongoDB SRV records
+
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hospital API is running...");
@@ -16,3 +28,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/api/users", userRoutes);
